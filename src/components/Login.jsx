1c84ce9331a87674d53d2b6e6d6b781218105b0f
Preethi Madhavan <98338783+preethimaddy@users.noreply.github.com>
2025-01-29 12:22:18 +0530

    login page

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -2,36 +2,36 @@ import {useState} from 'react'
 import './Login.css'
 const Login = () => {
 
-  const initialState = {
-    firstName: "",
-    lastName: "",
-    email: "",
-    gender: "",
-    
-  };
-  const [newInput, setNewInput] = useState(initialState);
-  console.log("newInput", newInput);
   
-  const {firstName, lastName, email, gender} = newInput;
+  const handleSubmit =()=>{
+
+  }
+  const [username, setUsername] = useState();
+  console.log("username", username);
+
+  const [password, setPassword] = useState();
+  console.log("Password", password);
+ 
   return (
     <div className='container'>
     <div className='loginCard'>
     <form>
     <div className="form-group">
-        <label htmlFor='email'>Enter your email ID : </label>
-        <input type='text' class="form-control" value={newInput.firstName} placeholder='abc@gmail.com'/>
+        <label htmlFor='email'>Enter your User email ID : </label>
+        <input type='text' onChange={e=> setUsername(e.target.value)} placeholder='abc@gmail.com'/>
+        <br />
         <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small>
     </div>
     <div className="form-group">
         <label  htmlFor='password'>Enter your Password : </label>
-        <input  type='password' class="form-control" value={newInput.lastName} placeholder='*****'/>
+        <input  type='password' onChange={e=> setPassword(e.target.value)} placeholder='*****'/>
     </div>
     <div class="form-group form-check">
     <label  htmlFor='checkbox'>
       <input class="form-check-input" type="checkbox" name="remember" /> Remember me
     </label>
   </div>
-  <button type="submit" class="btn btn-primary">Submit</button>
+  <button type="submit" onClick={handleSubmit} className="btn btn-primary">Submit</button>
  </form>
  </div>
  </div>
